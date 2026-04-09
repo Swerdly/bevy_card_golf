72dfdee2c13073a482a5bce54ed225b300eb849e
ErikBliach <erikbliacher777@gmail.com>
2026-04-08 21:44:23 -0400

    Added Deck struct, Added Card struct to encapsulate card enums

--- src/cards.rs
+++ src/cards.rs
@@ -101,6 +101,113 @@ impl CardType {
 
 }
 
+#[derive(Component, Clone, Copy)]
+pub struct Card{
+    value: i32,
+    ability: CardAbility,
+    pub card_type: CardType,
+}
+
+pub struct Deck{
+    pub cards: Vec<Card>,
+}
+
+impl Deck{
+
+    //Create card array at the beginning with every possible card
+    pub fn initialize_deck(&mut self){
+        let mut cards_in_deck = 0;
+
+        while cards_in_deck < 54 {
+            match cards_in_deck {
+                0..=3 => {
+                    let card = Card{ value: 1, ability: CardAbility::None, card_type: CardType::Ace };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                4..=7 => {
+                    let card = Card{ value: 2, ability: CardAbility::None, card_type: CardType::Two };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                8..=11 => {
+                    let card = Card{ value: 3, ability: CardAbility::None, card_type: CardType::Three };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                }
+                12..=15 => {
+                    let card = Card{ value: 4, ability: CardAbility::None, card_type: CardType::Four };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                16..=19 => {
+                    let card = Card{ value: 5, ability: CardAbility::None, card_type: CardType::Five };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                20..=23 => {
+                    let card = Card{ value: 6, ability: CardAbility::None, card_type: CardType::Six };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                24..=27 => {
+                    let card = Card{ value: 7, ability: CardAbility::SelfPeek, card_type: CardType::Seven };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                28..=31 => {
+                    let card = Card{ value: 8, ability: CardAbility::SelfPeek, card_type: CardType::Eight };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                32..=35 => {
+                    let card = Card{ value: 9, ability: CardAbility::ElsePeek, card_type: CardType::Nine };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                36..=39 => {
+                    let card = Card{ value: 10, ability: CardAbility::ElsePeek, card_type: CardType::Ten };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                40..=43 => {
+                    let card = Card{ value: 11, ability: CardAbility::BlindSwap, card_type: CardType::Jack };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                44..=47 => {
+                    let card = Card{ value: 12, ability: CardAbility::BlindSwap, card_type: CardType::Queen };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                48..=49 => {
+                    let card = Card{ value: 13, ability: CardAbility::PeekAndSwap, card_type: CardType::BlackKing };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                50..=51 => {
+                    let card = Card{ value: 0, ability: CardAbility::None, card_type: CardType::RedKing };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                52..=53 => {
+                    let card = Card{ value: -1, ability: CardAbility::None, card_type: CardType::Joker };
+                    self.cards.push(card);
+                    cards_in_deck += 1;
+                },
+                _ => {
+                    println!("silence rust");
+                },
+            }
+        }
+        let mut card_num = 1;
+        for item in &self.cards{
+            println!("{card_num}. {}", item.value);
+            card_num += 1;
+        }
+    }
+}
+
 pub fn spawn_card(
     commands: &mut Commands,
     asset_server: &AssetServer,
@@ -115,6 +222,11 @@ pub fn spawn_card(
     let ability = card_type.ability();
     let texture =  asset_server.load(  card_type.sprite());
 
+    let card = Card{
+        value: value,
+        ability: ability,
+        card_type: card_type,
+    };
     
     commands.spawn((
         Sprite {
@@ -123,9 +235,10 @@ pub fn spawn_card(
             ..default()
         },
         Transform::from_translation(translation),
-        card_type,                  
-        CardValue { value },        
-        ability,    
+        card,
+        // card_type,                  
+        // CardValue { value },        
+        // ability,    
         children![(
     
             Mesh2d(mesh),
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use cards::*;
 const CARD_HEIGHT: f32  = 105.0;
 const CARD_WIDTH: f32  = 75.0;
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
 #[derive(Component)]
 pub enum Owner{
@@ -25,8 +27,10 @@ fn main() {
             }),
             ..default()
         }))
+        .insert_resource(GreetTimer(Timer::from_seconds(0.5, TimerMode::Once)))
         .add_plugins(MeshPickingPlugin, )
         .add_systems(Startup, setup)
+        .add_systems(Update, queryTest)
         .run();
    
 }
@@ -37,12 +41,12 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
      asset_server: ResMut<AssetServer>,
 ){
-
+    let mut deck = Deck{cards: Vec::new()};
     let card_mesh = meshes.add(Rectangle::new(CARD_WIDTH,CARD_HEIGHT));
     let card_material = materials.add(Color::srgb(0.2, 0.7, 0.9));
 
     commands.spawn(Camera2d);
-
+    deck.initialize_deck();
     spawn_card(
         &mut commands, 
         &asset_server, 
@@ -51,4 +55,34 @@ fn setup(
         card_mesh,
         card_material
     );
+    let card_mesh = meshes.add(Rectangle::new(CARD_WIDTH,CARD_HEIGHT));
+    let card_material = materials.add(Color::srgb(0.2, 0.7, 0.9));
+    spawn_card(
+        &mut commands, 
+        &asset_server, 
+        CardType::Queen, 
+        Vec3::new(0.0, 0.0, 0.0),
+        card_mesh,
+        card_material
+    );
+    let card_mesh = meshes.add(Rectangle::new(CARD_WIDTH,CARD_HEIGHT));
+    let card_material = materials.add(Color::srgb(0.2, 0.7, 0.9));
+    spawn_card(
+        &mut commands, 
+        &asset_server, 
+        CardType::Joker, 
+        Vec3::new(0.0, 0.0, 0.0),
+        card_mesh,
+        card_material
+    );
+}
+
+fn queryTest(query: Query<&Card>, time: Res<Time>, mut timer: ResMut<GreetTimer>,){
+    if timer.0.tick(time.delta()).just_finished() {
+        for thing in query {
+            if let CardType::Joker = thing.card_type{
+                println!("joker here")
+            }
+        }
+    }
 }
